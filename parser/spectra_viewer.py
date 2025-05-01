e994e1f9bfc97f24de79ca104a432926e74e1914
Adrian Merino <adrian.fernando.merino@ut.ee>
2025-05-02 00:51:12 +0200

    feat:add webpage, add api to parser

---
+++ parser/spectra_viewer.py
@@ -0,0 +1,33 @@
+from brukeropusreader import read_file
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    return response
+
+@app.route("/parse_spectra", methods=["POST"])
+def parse_spectra():
+    try:
+        data = request.get_json()
+        if not data or "input" not in data:
+            return "Error: No input received"
+
+        print(data["input"])
+        print(type(data["input"]))
+        user_input = str(read_file(data["input"]))
+        print(user_input)
+        return jsonify({
+            "output": user_input,
+            "input_received": user_input
+        })
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=500, debug=True)
+
--- spectra_viewer.py
+++
@@ -1,4 +0,0 @@
-from brukeropusreader import read_file
-
-opus_data = read_file("L1 GommeJuly22_09h30m56s.0")
-print(opus_data)
